5673d34c76ef360d8e5242fb927e66d4ac8a8bab
vadim-hleif <vadzim.hleif@gmail.com>
2018-07-12 13:13:48 +0300

    Migrate readme yaml

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "id" {
-  value = "${aws_acm_certificate.default.id}"
+  value       = "${aws_acm_certificate.default.id}"
+  description = "The ARN of the certificate"
 }
 
 output "arn" {
-  value = "${aws_acm_certificate.default.arn}"
+  value       = "${aws_acm_certificate.default.arn}"
+  description = "The ARN of the certificate"
 }
 
 output "domain_validation_options" {
-  value = "${aws_acm_certificate.default.domain_validation_options}"
+  value       = "${aws_acm_certificate.default.domain_validation_options}"
+  description = "CNAME records that are added to the DNS zone to complete certificate validation"
 }
