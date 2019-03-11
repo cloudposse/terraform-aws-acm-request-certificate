b677d4af3b43b51c0cac62aab4eb0d08a23e4a8a
Jamie Nelson <stack@bitflight.io>
2019-03-12 08:57:56 +1300

    fixed the certificate requests (#9)
    * fixed the certificate requests
    
    * Sorted and made the subject alternative names unique
    
    * Sorted and made the subject alternative names unique

--- main.tf
+++ main.tf
@@ -1,8 +1,14 @@
+locals {
+  # Remove the var.domain_name from the subject_alternative_names as it doesn't need to be there.
+  sans                      = ["${sort(distinct(compact(var.subject_alternative_names)))}"]
+  subject_alternative_names = ["${concat( slice(local.sans, 0, index(local.sans, var.domain_name)), slice(local.sans, index(local.sans, var.domain_name) + 1 , length(local.sans) ))}"]
+}
+
 resource "aws_acm_certificate" "default" {
   count                     = "${var.enabled == "true" ? 1 : 0}"
   domain_name               = "${var.domain_name}"
   validation_method         = "${var.validation_method}"
-  subject_alternative_names = ["${var.subject_alternative_names}"]
+  subject_alternative_names = ["${local.subject_alternative_names}"]
   tags                      = "${var.tags}"
 
   lifecycle {
@@ -16,31 +22,20 @@ data "aws_route53_zone" "default" {
   private_zone = false
 }
 
-locals {
-  domain_validation_options = "${aws_acm_certificate.default.domain_validation_options[0]}"
-  zone_name                 = "${var.zone_name == "" ? var.domain_name : var.zone_name}"
-}
-
-resource "null_resource" "default" {
-  count = "${var.enabled == "true" && var.process_domain_validation_options == "true" && var.validation_method == "DNS" ? length(aws_acm_certificate.default.domain_validation_options) : 0}"
-
-  triggers = "${aws_acm_certificate.default.domain_validation_options[count.index]}"
-}
-
 resource "aws_acm_certificate_validation" "default" {
-  count           = "${var.enabled == "true" ? 1 : 0}"
+  count           = "${var.process_domain_validation_options == "true" ? 1 : 0}"
   certificate_arn = "${aws_acm_certificate.default.arn}"
 
   validation_record_fqdns = [
-    "${distinct(compact(concat(aws_route53_record.default.fqdn, var.subject_alternative_names)))}",
+    "${distinct(compact(aws_route53_record.default.*.fqdn))}",
   ]
 }
 
 resource "aws_route53_record" "default" {
-  count   = "${length(null_resource.default.triggers)}"
+  count   = "${var.process_domain_validation_options == "true" && var.validation_method == "DNS" ? length(local.subject_alternative_names)  + 1 : 0 }"
   zone_id = "${data.aws_route53_zone.default.zone_id}"
-  name    = "${lookup("null_resource.default.${count.index}","resource_record_name")}"
-  type    = "${lookup("null_resource.default.${count.index}", "resource_record_type")}"
+  name    = "${lookup(aws_acm_certificate.default.domain_validation_options[count.index],"resource_record_name")}"
+  type    = "${lookup(aws_acm_certificate.default.domain_validation_options[count.index], "resource_record_type")}"
   ttl     = "${var.ttl}"
-  records = ["${lookup("null_resource.default.${count.index}","resource_record_value")}"]
+  records = ["${lookup(aws_acm_certificate.default.domain_validation_options[count.index],"resource_record_value")}"]
 }
