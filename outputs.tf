e0285b75dd9169d7ede5fe60b3980e7d7794c01e
Igor Rodionov <goruha@gmail.com>
2019-03-12 16:13:46 +0600

    Test EMAIL validation

--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,7 @@ output "id" {
 }
 
 output "arn" {
-  value       = "${join("",aws_acm_certificate_validation.dns.*.certificate_arn)}"
+  value       = "${join("",aws_acm_certificate.default.*.arn)}"
   description = "The ARN of the certificate"
 }
 
@@ -12,3 +12,10 @@ output "domain_validation_options" {
   value       = ["${aws_acm_certificate.default.*.domain_validation_options}"]
   description = "CNAME records that are added to the DNS zone to complete certificate validation"
 }
+
+output "email_validation_options" {
+  value       = ["${aws_acm_certificate.default.*.validation_emails}"]
+  description = " A list of addresses that received a validation E-Mail"
+}
+
+
