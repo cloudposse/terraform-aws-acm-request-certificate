6c27c2196f813b332e2d4e1ffd37288ce06c6842
Igor Rodionov <goruha@gmail.com>
2019-03-15 17:28:04 +0600

    Fix dns records

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 locals {
-  zone_name                = "${var.zone_name == "" ? var.domain_name : var.zone_name}"
-  validation_enabled       = "${var.enabled == "true" && var.process_domain_validation_options == "true" ? true : false}"
-  dns_validation_enabled   = "${local.validation_enabled == "true" && var.validation_method == "DNS" ? true : false}"
-  domains                  = ["${concat(list(var.domain_name), var.subject_alternative_names)}"]
-  unique_domains           = ["${distinct(compact(split(" ",replace(join(" ", local.domains), "*.", ""))))}"]
+  zone_name              = "${var.zone_name == "" ? var.domain_name : var.zone_name}"
+  validation_enabled     = "${var.enabled == "true" && var.process_domain_validation_options == "true" ? true : false}"
+  dns_validation_enabled = "${local.validation_enabled == "true" && var.validation_method == "DNS" ? true : false}"
+  domains                = ["${concat(list(var.domain_name), var.subject_alternative_names)}"]
+  unique_domains         = ["${distinct(compact(split(" ",replace(join(" ", local.domains), "*.", ""))))}"]
 }
 
 resource "aws_acm_certificate" "default" {
@@ -21,9 +21,10 @@ data "aws_route53_zone" "default" {
 }
 
 resource "null_resource" "dns_records" {
-  count   = "${local.dns_validation_enabled ? length(local.domains) : 0 }"
+  count = "${local.dns_validation_enabled ? length(local.domains) : 0 }"
+
   triggers {
-    record  = "${format("%s:%s:%s", lookup(flatten(aws_acm_certificate.default.*.domain_validation_options)[count.index], "resource_record_name", ""), lookup(flatten(aws_acm_certificate.default.*.domain_validation_options)[count.index], "resource_record_type", ""), lookup(flatten(aws_acm_certificate.default.*.domain_validation_options)[count.index], "resource_record_value", ""))}"
+    record = "${format("%s:%s:%s", lookup(aws_acm_certificate.default.*.domain_validation_options[count.index], "resource_record_name", ""), lookup(aws_acm_certificate.default.*.domain_validation_options[count.index], "resource_record_type", ""), lookup(aws_acm_certificate.default.*.domain_validation_options[count.index], "resource_record_value", ""))}"
   }
 }
 
