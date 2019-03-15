7158557d8e4aa4f703cac854da517e3b2dc6648a
Igor Rodionov <goruha@gmail.com>
2019-03-15 17:37:14 +0600

    Fix dns records

--- main.tf
+++ main.tf
@@ -14,36 +14,36 @@ resource "aws_acm_certificate" "default" {
   tags                      = "${var.tags}"
 }
 
-data "aws_route53_zone" "default" {
-  count        = "${local.dns_validation_enabled ? 1 : 0}"
-  name         = "${local.zone_name}"
-  private_zone = false
-}
-
-resource "null_resource" "dns_records" {
-  count = "${local.dns_validation_enabled ? length(local.domains) : 0 }"
-
-  triggers {
-    record = "${format("%s:%s:%s", lookup(aws_acm_certificate.default.*.domain_validation_options[count.index], "resource_record_name", ""), lookup(aws_acm_certificate.default.*.domain_validation_options[count.index], "resource_record_type", ""), lookup(aws_acm_certificate.default.*.domain_validation_options[count.index], "resource_record_value", ""))}"
-  }
-}
-
-resource "aws_route53_record" "default" {
-  count   = "${local.dns_validation_enabled ? length(local.unique_domains) : 0 }"
-  zone_id = "${data.aws_route53_zone.default.zone_id}"
-  name    = "${element(split(":", element(distinct(null_resource.dns_records.*.triggers.record), count.index)), 0)}"
-  type    = "${element(split(":", element(distinct(null_resource.dns_records.*.triggers.record), count.index)), 1)}"
-  records = ["${element(split(":", element(distinct(null_resource.dns_records.*.triggers.record), count.index)), 2)}"]
-  ttl     = "${var.ttl}"
-
-  depends_on = ["null_resource.dns_records"]
-}
-
-resource "aws_acm_certificate_validation" "dns" {
-  count           = "${local.dns_validation_enabled ? 1 : 0}"
-  certificate_arn = "${aws_acm_certificate.default.arn}"
-
-  validation_record_fqdns = [
-    "${aws_route53_record.default.*.fqdn}",
-  ]
-}
+//data "aws_route53_zone" "default" {
+//  count        = "${local.dns_validation_enabled ? 1 : 0}"
+//  name         = "${local.zone_name}"
+//  private_zone = false
+//}
+//
+//resource "null_resource" "dns_records" {
+//  count = "${local.dns_validation_enabled ? length(local.domains) : 0 }"
+//
+//  triggers {
+//    record = "${format("%s:%s:%s", lookup(aws_acm_certificate.default.*.domain_validation_options[count.index], "resource_record_name", ""), lookup(aws_acm_certificate.default.*.domain_validation_options[count.index], "resource_record_type", ""), lookup(aws_acm_certificate.default.*.domain_validation_options[count.index], "resource_record_value", ""))}"
+//  }
+//}
+//
+//resource "aws_route53_record" "default" {
+//  count   = "${local.dns_validation_enabled ? length(local.unique_domains) : 0 }"
+//  zone_id = "${data.aws_route53_zone.default.zone_id}"
+//  name    = "${element(split(":", element(distinct(null_resource.dns_records.*.triggers.record), count.index)), 0)}"
+//  type    = "${element(split(":", element(distinct(null_resource.dns_records.*.triggers.record), count.index)), 1)}"
+//  records = ["${element(split(":", element(distinct(null_resource.dns_records.*.triggers.record), count.index)), 2)}"]
+//  ttl     = "${var.ttl}"
+//
+//  depends_on = ["null_resource.dns_records"]
+//}
+//
+//resource "aws_acm_certificate_validation" "dns" {
+//  count           = "${local.dns_validation_enabled ? 1 : 0}"
+//  certificate_arn = "${aws_acm_certificate.default.arn}"
+//
+//  validation_record_fqdns = [
+//    "${aws_route53_record.default.*.fqdn}",
+//  ]
+//}
