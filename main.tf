7b308f175b8220175dfbf9ecbcf81bac088be3fa
Igor Rodionov <goruha@gmail.com>
2019-03-15 15:06:38 +0600

    Fix dns records

--- main.tf
+++ main.tf
@@ -15,10 +15,6 @@ resource "aws_acm_certificate" "default" {
   validation_method         = "${var.validation_method}"
   subject_alternative_names = ["${var.subject_alternative_names}"]
   tags                      = "${var.tags}"
-
-  lifecycle {
-    create_before_destroy = true
-  }
 }
 
 data "aws_route53_zone" "default" {
