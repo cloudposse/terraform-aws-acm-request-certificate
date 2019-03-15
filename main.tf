c479ac084f7477988f6aaab82485b8e20a68d50f
Igor Rodionov <goruha@gmail.com>
2019-03-15 13:42:02 +0600

    Fix dns records

--- main.tf
+++ main.tf
@@ -5,8 +5,6 @@ locals {
   dns_validation_records   = ["${flatten(aws_acm_certificate.default.*.domain_validation_options)}"]
   domains                  = ["${concat(list(var.domain_name), var.subject_alternative_names)}"]
   unique_domains           = ["${distinct(compact(split(" ",replace(join(" ", local.domains), "*.", ""))))}"]
-
-  test  =
 }
 
 resource "aws_acm_certificate" "default" {
