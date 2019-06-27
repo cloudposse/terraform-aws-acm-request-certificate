22c25042ec1d94f2e787ed0f501077cdc7a520f5
aknysh <andriy.knysh@gmail.com>
2019-06-27 15:12:56 -0400

    Convert to TF 0.12. Add tests. Add Codefresh test pipeline

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-locals {
-  zone_name                                    = var.zone_name == "" ? var.domain_name : var.zone_name
-  process_domain_validation_options            = var.enabled && var.process_domain_validation_options && var.validation_method == "DNS"
-  domain_validation_options_list               = local.process_domain_validation_options ? aws_acm_certificate.default.0.domain_validation_options : []
-  subject_alternative_names_has_star_subdomain = contains(var.subject_alternative_names, "*.${var.domain_name}")
-  subject_alternative_names_count              = length(var.subject_alternative_names)
-  need_validation_record_for_domain            = local.subject_alternative_names_count == 0 || local.subject_alternative_names_has_star_subdomain == false
-  validation_record_count                      = local.need_validation_record_for_domain ? local.subject_alternative_names_count + 1 : local.subject_alternative_names_count
-}
-
 resource "aws_acm_certificate" "default" {
   count                     = var.enabled ? 1 : 0
   domain_name               = var.domain_name
@@ -20,6 +10,12 @@ resource "aws_acm_certificate" "default" {
   }
 }
 
+locals {
+  zone_name                         = var.zone_name == "" ? var.domain_name : var.zone_name
+  process_domain_validation_options = var.enabled && var.process_domain_validation_options && var.validation_method == "DNS"
+  domain_validation_options_list    = local.process_domain_validation_options ? aws_acm_certificate.default.0.domain_validation_options : []
+}
+
 data "aws_route53_zone" "default" {
   count        = local.process_domain_validation_options ? 1 : 0
   name         = "${local.zone_name}."
@@ -27,12 +23,13 @@ data "aws_route53_zone" "default" {
 }
 
 resource "aws_route53_record" "default" {
-  count   = local.process_domain_validation_options ? local.validation_record_count : 0
-  zone_id = join("", data.aws_route53_zone.default.*.zone_id)
-  ttl     = var.ttl
-  name    = lookup(local.domain_validation_options_list[count.index], "resource_record_name")
-  type    = lookup(local.domain_validation_options_list[count.index], "resource_record_type")
-  records = [lookup(local.domain_validation_options_list[count.index], "resource_record_value")]
+  count           = local.process_domain_validation_options ? length(var.subject_alternative_names) + 1 : 0
+  zone_id         = join("", data.aws_route53_zone.default.*.zone_id)
+  ttl             = var.ttl
+  allow_overwrite = true
+  name            = lookup(local.domain_validation_options_list[count.index], "resource_record_name")
+  type            = lookup(local.domain_validation_options_list[count.index], "resource_record_type")
+  records         = [lookup(local.domain_validation_options_list[count.index], "resource_record_value")]
 }
 
 resource "aws_acm_certificate_validation" "default" {
