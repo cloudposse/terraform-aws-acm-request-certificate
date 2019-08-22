f3106186924b78a454be5efe451bb7988ae9c6a5
Nuru <Nuru@users.noreply.github.com>
2019-08-21 18:49:48 -0700

    Revert bad merge

--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,7 @@ output "id" {
 }
 
 output "arn" {
-  value       = "${aws_acm_certificate_validation.default.certificate_arn}"
+  value       = "${aws_acm_certificate.default.arn}"
   description = "The ARN of the certificate"
 }
 
