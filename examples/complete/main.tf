9cb95ae3ac0535818941b32e66fb07418ba943f9
Maxim Mironenko <maxim@cloudposse.com>
2021-02-04 13:57:15 +0700

    Terraform 0.14 upgrade (#36)
    * Terraform 0.14 upgrade
    
    * workflows updated

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -5,11 +5,10 @@ provider "aws" {
 module "zone" {
   source           = "cloudposse/route53-cluster-zone/aws"
   version          = "0.12.0"
-  namespace        = var.namespace
-  stage            = var.stage
-  name             = var.name
   parent_zone_name = var.parent_zone_name
   zone_name        = "$${name}.$${parent_zone_name}"
+
+  context = module.this.context
 }
 
 module "acm_request_certificate" {
@@ -20,4 +19,6 @@ module "acm_request_certificate" {
   subject_alternative_names         = ["*.${module.zone.zone_name}"]
   process_domain_validation_options = var.process_domain_validation_options
   wait_for_certificate_issued       = var.wait_for_certificate_issued
+
+  context = module.this.context
 }
