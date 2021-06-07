ffc0be68ec14829342b6fd43e2586df5bd8f9281
cloudpossebot <11232728+cloudpossebot@users.noreply.github.com>
2021-06-07 02:29:18 +0000

    Auto Format

--- variables.tf
+++ variables.tf
@@ -7,9 +7,9 @@ variable "wait_for_certificate_issued" {
 variable "domain_name" {
   type        = string
   description = "A domain name for which the certificate should be issued"
-  
+
   validation {
-    condition     = !can(regex("[A-Z]", var.domain_name))
+    condition     = ! can(regex("[A-Z]", var.domain_name))
     error_message = "Domain name must be lower-case."
   }
 }
