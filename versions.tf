7b5f3438aca93a5f3c941dc6685789f89e1b6533
Yonatan Koren <me@yonatankoren.com>
2021-09-12 23:11:04 -0400

    Chore: update `context.tf`, `build-harness` assets (#50)
    Co-authored-by: cloudpossebot <11232728+cloudpossebot@users.noreply.github.com>

--- versions.tf
+++ versions.tf
@@ -1,18 +1,10 @@
 terraform {
-  required_version = ">= 0.12.26"
+  required_version = ">= 0.13"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 3.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 1.2"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 2.0"
     }
   }
 }
