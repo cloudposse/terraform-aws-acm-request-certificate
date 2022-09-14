d64d26e8d100d9fefeb0816ea0e1d3b28bbdf204
tbpoetke <stralau@freenet.de>
2022-09-14 19:58:54 +0200

    adding validation certificate_arn output (#59)
    * adding output for certification status
    
    * adding output of the validated arn
    
    * remove obsolete certstatus output
    
    * remove newline
    
    * Auto Format
    
    * Update outputs.tf
    
    Co-authored-by: Andriy Knysh <aknysh@users.noreply.github.com>
    
    * Auto Format
    
    * Update outputs.tf
    
    Co-authored-by: Andriy Knysh <aknysh@users.noreply.github.com>
    
    * Auto Format
    
    * add versions.tf to examples
    
    * adding output to the examples
    
    Co-authored-by: Tommy <thpoetke@testo.de>
    Co-authored-by: cloudpossebot <11232728+cloudpossebot@users.noreply.github.com>
    Co-authored-by: Andriy Knysh <aknysh@users.noreply.github.com>
    Co-authored-by: Tommy <thomas.poetke@exconcept.com>

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -32,3 +32,8 @@ output "certificate_domain_validation_options" {
   value       = module.acm_request_certificate.domain_validation_options
   description = "CNAME records that are added to the DNS zone to complete certificate validation"
 }
+
+output "validation_certificate_arn" {
+  value       = module.acm_request_certificate.validation_certificate_arn
+  description = "Certificate ARN from the `aws_acm_certificate_validation` resource"
+}
