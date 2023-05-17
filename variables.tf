e49df9a2078b7585264d1cf4154228a49dde9986
Max Lobur <max_lobur@outlook.com>
2023-05-17 12:48:57 +0300

    Sync github

--- variables.tf
+++ variables.tf
@@ -9,7 +9,7 @@ variable "domain_name" {
   description = "A domain name for which the certificate should be issued"
 
   validation {
-    condition     = ! can(regex("[A-Z]", var.domain_name))
+    condition     = !can(regex("[A-Z]", var.domain_name))
     error_message = "Domain name must be lower-case."
   }
 }
