5d7bb689ac1ac205c5394449a816db3b5f377061
Igor Rodionov <goruha@gmail.com>
2025-07-30 22:08:27 +0300

    Update examples_complete_test.go

--- test/src/examples_complete_test.go
+++ test/src/examples_complete_test.go
@@ -56,7 +56,7 @@ func TestExamplesComplete(t *testing.T) {
 	// Run `terraform output` to get the value of an output variable
 	zoneName := terraform.Output(t, terraformOptions, "zone_name")
 
-	expectedZoneName := "test-zone-2.testing.cloudposse.co"
+	expectedZoneName := "test-zone-2.modules.cptest.test-automation.app"
 	// Verify we're getting back the outputs we expect
 	assert.Equal(t, expectedZoneName, zoneName)
 
